a1f36f6649b7a108fb1ec52e502ccd94cbcc4c17
Florian Haas <florian@hastexo.com>
2017-06-08 13:10:24 +0000

    Add test for markdown method

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,17 +3,43 @@ from olxutils import helpers
 from unittest import TestCase
 
 
-class SuffixTest(TestCase):
+class OLXHelpersTestCase(TestCase):
+
+    def setUp(self):
+        self.h = helpers.OLXHelpers()
+
+
+class SuffixTest(OLXHelpersTestCase):
 
     def test_expected_suffix(self):
         expected = (('foo', u' (foo)'),
                     ('spam eggs', u' (spam eggs)'),
                     (u'', u''),
                     ('', u''),
-                    (None, u''))
+                    (None, u''),
+                    (False, u''))
         for t in expected:
-            self.assertEqual(helpers.suffix(t[0]),
+            self.assertEqual(self.h.suffix(t[0]),
                              t[1])
-                                            
 
 
+class MarkdownTest(OLXHelpersTestCase):
+
+    def test_basic_markdown(self):
+        extras = []
+        expected = (('**foo**', u'<p><strong>foo</strong></p>\n'),
+                    ('*bar*', u'<p><em>bar</em></p>\n'),
+                    ('*baz*', u'<p><em>baz</em></p>\n'))
+
+        for t in expected:
+            self.assertEqual(self.h.markdown(t[0], extras),
+                             t[1])
+
+    def test_strip_whitespace(self):
+        extras = []
+        expected = (('\n*bar*', u'<p><em>bar</em></p>\n'),
+                    ('\nspam', u'<p>spam</p>\n'))
+
+        for t in expected:
+            self.assertEqual(self.h.markdown(t[0], extras),
+                             t[1])
