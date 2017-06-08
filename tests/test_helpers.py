b99293742ea96283a4c7e9b44b31b31ae696ab58
Florian Haas <florian@hastexo.com>
2017-06-08 16:10:18 +0200

    Test markdown method with default extras, and with no extras

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -26,22 +26,28 @@ class SuffixTest(OLXHelpersTestCase):
 class MarkdownTest(OLXHelpersTestCase):
 
     def test_basic_markdown(self):
-        extras = []
         expected = (('**foo**', u'<p><strong>foo</strong></p>\n'),
                     ('*bar*', u'<p><em>bar</em></p>\n'),
                     ('*baz*', u'<p><em>baz</em></p>\n'))
 
         for t in expected:
-            self.assertEqual(self.h.markdown(t[0], extras),
+            # No extras
+            self.assertEqual(self.h.markdown(t[0], extras=[]),
+                             t[1])
+            # Default set of extras
+            self.assertEqual(self.h.markdown(t[0]),
                              t[1])
 
     def test_strip_whitespace(self):
-        extras = []
         expected = (('\n*bar*', u'<p><em>bar</em></p>\n'),
                     ('\nspam', u'<p>spam</p>\n'),
                     ('\n*bar*   ', u'<p><em>bar</em></p>\n'),
                     ('\nspam    ', u'<p>spam</p>\n'))
 
         for t in expected:
-            self.assertEqual(self.h.markdown(t[0], extras),
+            # No extras
+            self.assertEqual(self.h.markdown(t[0], extras=[]),
+                             t[1])
+            # Default set of extras
+            self.assertEqual(self.h.markdown(t[0]),
                              t[1])
