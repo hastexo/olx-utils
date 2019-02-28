0772455778b28c97010322e835a78732d7ceac46
Florian Haas <xahteiwi@gmail.com>
2019-02-28 19:23:50 +0100

    Fix broken tests
    Two invocations of self.assertRaises contained both an incorrect
    indentation (meaning the test didn't run), and an incorrect use
    of the exception context (meaning the test would have failed even
    if the indentation had been corrected).

--- tests/test_git.py
+++ tests/test_git.py
@@ -87,8 +87,8 @@ class GitHelperTestCase(TestCase):
                         {'PATH': ''}):
             with self.assertRaises(GitHelperException) as e:
                 self.helper.create_branch()
-                expected_msg = 'Error creating branch run/%s' % self.RUN_NAME
-                self.assertEquals(str(e), expected_msg)
+            expected_msg = 'Error creating branch run/%s' % self.RUN_NAME
+            self.assertEquals(str(e.exception), expected_msg)
 
     def test_create_branch_again(self):
         """
@@ -139,8 +139,8 @@ class GitHelperTestCase(TestCase):
                         {'PATH': ''}):
             with self.assertRaises(GitHelperException) as e:
                 self.helper.add_to_branch()
-                expected_msg = 'Error committing new run.'
-                self.assertEquals(str(e), expected_msg)
+            expected_msg = 'Error committing new run.'
+            self.assertEquals(str(e.exception), expected_msg)
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
